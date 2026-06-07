b402b97b801506fa2ec3f26f67af1d7d7a4fbd26
fleajion <fleajion@users.noreply.github.com>
2026-06-07 11:05:50 -0500

    Configure Next.js static export and GitHub Pages deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,34 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/github",
-        destination: "https://github.com/fleajion",
-        permanent: true,
-      },
-      {
-        source: "/linkedin",
-        destination: "https://www.linkedin.com/in/zidan-naufal-firmansyah/",
-        permanent: true,
-      },
-      {
-        source: "/youtube",
-        destination: "https://www.youtube.com/@mrjhons5512",
-        permanent: true,
-      },
-      {
-        source: "/instagram",
-        destination: "https://www.instagram.com/zid4n_np",
-        permanent: true
-      },
-      {
-        source: "/medium",
-        destination: "https://medium.com/@zidannfyourbae999",
-        permanent: true
-      }
-    ];
+  images: {
+    unoptimized: true,
   },
 };
 
